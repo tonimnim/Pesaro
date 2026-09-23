26d5a518f8fc953829708d6ea318d1c2854f003d
Tonimnim <k130581708@gmail.com>
2026-09-24 01:51:18 +0300

    feat(operations): scaffold rails, treasury, reconciliation and cases

---
+++ services/treasury/cmd/treasury/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"os"
+
+	"github.com/tonimnim/Pesaro/internal/platform/service"
+	"github.com/tonimnim/Pesaro/services/treasury/internal/app"
+)
+
+func main() {
+	os.Exit(service.Process(app.Run))
+}
